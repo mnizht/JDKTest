ed0748253edcfb0c303308baf75ff87a0e4df127
zhuhaitao <mnizht@qq.com>
2019-08-09 09:40:50 +0800

    fix bug

--- src/cn/zhuht/jdk8test/pojo/NodeSort1.java
+++ src/cn/zhuht/jdk8test/pojo/NodeSort1.java
@@ -1,6 +1,7 @@
 package cn.zhuht.jdk8test.pojo;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 /**
@@ -8,6 +9,7 @@ import lombok.experimental.Accessors;
  * @date 2019/7/11 11:15
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 public class NodeSort1 extends NodeBase {
   private String name;
